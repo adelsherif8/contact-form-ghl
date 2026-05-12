9fa52762017eb6620192b405cab1d9ecabc5d11a
Adel Emad <adelsherif8@gmail.com>
2026-05-12 08:42:07 +0300

    Release v2.6.9

--- contact-form-ghl.php
+++ contact-form-ghl.php
@@ -3,7 +3,7 @@
  * Plugin Name: Contact Form + GoHighLevel
  * Plugin URI: https://upwork.com/freelancers/adelsherif8
  * Description: Fully customizable contact form with GoHighLevel CRM integration. Use shortcode [contact_form_ghl].
- * Version:     2.6.8
+ * Version:     2.6.9
  * Author:      Adel Emad
  * Author URI:  https://upwork.com/freelancers/adelsherif8
  * License:     GPL-2.0+
